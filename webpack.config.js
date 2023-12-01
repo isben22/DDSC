a052f37167c50937267768744c380cd4d6b84921
isben22@gmail.com <isbn22@gmail.com>
2023-11-30 19:46:46 -0500

    configs2

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js', // Your main JavaScript file
+  entry: './index.html', // Your main JavaScript file
   output: {
     filename: 'bundle.js', // The output bundle
     path: path.resolve(__dirname, 'dist'),
