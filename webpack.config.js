3cea14d25bd3375c323293824ef6d49a4ce19234
isben22@gmail.com <isbn22@gmail.com>
2023-11-30 20:14:24 -0500

    webp

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './index.js', // Your entry point
+  entry: './index.html', // Your entry point
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html' // Path to your HTML file
+      template: './index.html' // Path to your HTML file
     })
   ],
   // ... other configurations like loaders
